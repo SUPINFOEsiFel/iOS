831317e0129f7b541dfbebc6497511ac6c031a06
MehdiZ <mehdi.zannettacci@hotmail.fr>
2015-04-15 20:02:28 +0200

    SlideOut / Ecran Chargement + Logo FEL

---
+++ Aria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aria
+//
+//  Created by Aria Company on 15/04/2015.
+//  Copyright (c) 2015 Aria Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
