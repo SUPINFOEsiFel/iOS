3d1c6a1f95dab6a5b1b54515ef1ae08692e4aeae
BEAL Corentin <coco.bea@hotmail.fr>
2015-07-05 11:46:09 +0200

    Ajout de liens vers les résidences studelites

--- SlideoutMenu/Second.swift
+++ SlideoutMenu/Second.swift
@@ -12,8 +12,25 @@ class Second : UIViewController {
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
     }
     
-    @IBAction func OnImageClick(sender: AnyObject) {
-        let testUrl = NSURL(string: "http://www.studelites.com/fr/lyon/");
-        UIApplication.sharedApplication().openURL(testUrl!);
+    @IBAction func ButtonBron(sender: UIButton) {
+        self.GoToWebsite("39280/bron-lumiere-bron/detail");
+    }
+    
+    @IBAction func ButtonDali(sender: UIButton) {
+        self.GoToWebsite("36724/lyon/residence-etudiante-dali-ii-lyon-3.html");
+    }
+    
+    @IBAction func ButtonDufy(sender: UIButton) {
+        self.GoToWebsite("36853/lyon/residence-etudiante-dufy-villeurbanne.html");
+    }
+    
+    @IBAction func ButtonLouis(sender: UIButton) {
+        self.GoToWebsite("36770/lyon/residence-etudiante-saint-louis-lyon-7.html");
+    }
+    
+    func GoToWebsite(name: String){
+        let base_url = "http://m.studelites.com/#!fr/residence/";
+        let dest_url = NSURL(string: base_url + name);
+        UIApplication.sharedApplication().openURL(dest_url!);
     }
 }
